e9e4064927dbfb2dc8f20361fe943c81fed1e98c
jarmo <jarmoil@metropolia.fi>
2025-03-25 13:40:11 +0200

    Implement restaurant listing and details modal; add fetch data functionality

---
+++ apiFetchData.js
@@ -0,0 +1,114 @@
+import {fetchData} from '../lib/fetchData.js';
+// your code here
+
+const apiUrl = 'https://media2.edu.metropolia.fi/restaurant/api/v1';
+const taulukko = document.querySelector('#target');
+const modal = document.querySelector('#modal');
+let restaurants = [];
+
+// html funktiot
+function createRestaurantCells(restaurant, tr) {
+  // nimisolu
+  const nameTd = document.createElement('td');
+  nameTd.innerText = restaurant.name;
+  // osoitesolu
+  const addressTd = document.createElement('td');
+  addressTd.innerText = restaurant.address;
+  //kaupunkisolu
+  const cityTd = document.createElement('td');
+  cityTd.innerText = restaurant.city;
+  tr.append(nameTd, addressTd, cityTd);
+}
+
+function createModalHtml(restaurant, modal) {
+  const nameH3 = document.createElement('h3');
+  nameH3.innerText = restaurant.name;
+  const addressP = document.createElement('p');
+  addressP.innerText = `${restaurant.address}, puhelin: ${restaurant.phone}`;
+  modal.append(nameH3, addressP);
+}
+
+function createMenuHtml(courses, modal) {
+  let html = '';
+  for (const course of courses) {
+    html += `
+      <article class="course">
+          <p><strong>${course.name}</strong>,
+          Hinta: ${course.price},
+          Allergeenit: ${course.diets}</p>
+      <article>
+    `;
+  }
+  return html;
+}
+
+// hae kaikki ravintolat
+async function getRestaurants() {
+  try {
+    restaurants = await fetchData(apiUrl + '/restaurants');
+  } catch (error) {
+    console.error(error);
+  }
+}
+// hae tietyn ravintolan päivän menu
+
+async function getDailyMenu(id, lang) {
+  try {
+    return await fetchData(`${apiUrl}/restaurants/daily/${id}/${lang}`);
+  } catch {
+    console.log(error);
+  }
+}
+
+let edellinenHighlight;
+
+// restaurants aakkosjärjestykseen
+function sortRestaurants() {
+  restaurants.sort((a, b) => {
+    return a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1;
+  });
+}
+
+function createTable() {
+  for (const restaurant of restaurants) {
+    // rivi
+    const tr = document.createElement('tr');
+    tr.addEventListener('click', async function () {
+      try {
+        edellinenHighlight?.classList.remove('highlight');
+        tr.classList.add('highlight');
+        edellinenHighlight = tr;
+
+        // hae menu
+        const coursesResponse = await getDailyMenu(restaurant._id, 'fi');
+        // hae menu html
+        const menuHtml = createMenuHtml(coursesResponse.courses);
+        // tyhjennä modal
+        modal.innerHTML = '';
+        // avaa modal
+        modal.showModal();
+        // tee modalin sisältö
+        createModalHtml(restaurant, modal);
+        // lisää menu html
+        modal.insertAdjacentHTML('beforeend', menuHtml);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+    // lisätään solut riviin
+    createRestaurantCells(restaurant, tr);
+    taulukko.append(tr);
+  }
+}
+
+async function main() {
+  try {
+    await getRestaurants();
+    sortRestaurants();
+    createTable();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
